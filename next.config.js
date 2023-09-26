c82bd764980f27817022c632c48fb46668c11603
abdelrahman134 <104602156+abdelrahman134@users.noreply.github.com>
2023-09-27 01:28:14 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript:{
+    ignoreDevErrors: true
+
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
