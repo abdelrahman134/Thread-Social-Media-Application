734a5d9df9eda09e10919324ab3b877e189edb8b
abdelrahman134 <104602156+abdelrahman134@users.noreply.github.com>
2023-09-28 20:51:17 +0300

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,10 +7,10 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const result =await fetchPosts(1,30)
   const user=await currentUser()
-    if (!user) return null;
-    const userInfo = await fetchUser(user.id);
+    // if (!user) return null;
+    // const userInfo = await fetchUser(user.id);
 
-    if (!userInfo?.onboarded) redirect("/onboarding");
+    // if (!userInfo?.onboarded) redirect("/onboarding");
  return (
     <div>
     <h1 className="head-text text-left">Home</h1>
