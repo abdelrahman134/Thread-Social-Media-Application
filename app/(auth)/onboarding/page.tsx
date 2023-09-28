a5985b17c4740a7fbe2a331d9fada0724fe396e1
abdelrahman134 <104602156+abdelrahman134@users.noreply.github.com>
2023-09-28 20:46:22 +0300

    first commit

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -9,15 +9,14 @@ const user = await currentUser();
 if(!user) return null
 const userInfo=await fetchUser(user.id)
 if(userInfo?.onboarding) redirect('/')
-const userData={
-  id:user?.id,
-  objectId:userInfo?._id,
-  username:userInfo?userInfo?.username :user?.username,
-  name:userInfo?.name || user?.firstName||"",
-  bio:userInfo?.bio||"",
-  image:userInfo.image || user?.imageUrl
-
-}
+const userData = {
+  id: user.id,
+  objectId: userInfo?._id,
+  username: userInfo ? userInfo?.username : user.username,
+  name: userInfo ? userInfo?.name : user.firstName ?? "",
+  bio: userInfo ? userInfo?.bio : "",
+  image: userInfo ? userInfo?.image : user.imageUrl,
+};
   return (
    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
     <h1 className='head-text'>Onboarding</h1>
